8464ac0f9b0cc31ea22cc0018f67d9ba89a77638
PetitSuave <xsimov01@gmail.com>
2025-01-28 20:57:26 +0100

    Deleted: Buttons on offer cards

--- app/components/offer-card.tsx
+++ app/components/offer-card.tsx
@@ -7,7 +7,9 @@ export type OfferCardProps = {
 export function OfferCard({title, text, link}: OfferCardProps) {
     return (
         <div
-            className="relative min-h-full border-solid border-teal-600 border-2 m-2 p-6 bg-gradient-to-b from-teal-50 to-teal-200"
+            className="relative min-h-full border-solid border-teal-600 bg-teal-100 border-2 m-2 p-6
+             {/*bg-gradient-to-b from-teal-50 to-teal-200 border-teal-600*/}
+             "
         >
             <div className="text-center mt-3 font-bold">
                 {title}
@@ -15,13 +17,13 @@ export function OfferCard({title, text, link}: OfferCardProps) {
             <div className="mt-6 mb-16">
                 {text}
             </div>
-            <div className="absolute flex justify-center items-center bottom-9 mx-auto inset-x-0">
-                <a href={link}>
-                    <button className="rounded-none bg-black text-white p-2 hover:bg-gray-600 pl-6 pr-6">
-                        Zjistit více
-                    </button>
-                </a>
-            </div>
+            {/*<div className="absolute flex justify-center items-center bottom-9 mx-auto inset-x-0">*/}
+            {/*    <a href={link}>*/}
+            {/*        <button className="rounded-none bg-black text-white p-2 hover:bg-gray-600 pl-6 pr-6">*/}
+            {/*            Zjistit více*/}
+            {/*        </button>*/}
+            {/*    </a>*/}
+            {/*</div>*/}
         </div>
     )
 }
